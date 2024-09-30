305def3e1f82d4350eb817d8a6c0e8702a50b32b
Manjunatha Sarma Majety <manjunathasarmamajety@gmail.com>
2024-09-30 19:26:11 +0530

    First commit

---
+++ encryption.c
@@ -0,0 +1,13 @@
+// My first try at a personal encryption software
+// By no definition is this a proper encryption program, but well its the beginning of a pursuit
+
+#include<stdio.h>
+
+
+
+int main()
+{
+ 
+
+
+}	
