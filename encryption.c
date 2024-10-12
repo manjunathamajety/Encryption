15fa236ea866c0c38da066a6f4f80f631abd829c
Manjunatha Sarma Majety <manjunathasarmamajety@gmail.com>
2024-10-12 21:40:05 +0530

    Could get the program to read a sentence

--- encryption.c
+++ encryption.c
@@ -1,5 +1,6 @@
 // My first try at a personal encryption software
 // By no definition is this a proper encryption program, but well its the beginning of a pursuit
+//This is my first program, to replace the letters of a sentence with different letters.
 
 #include<stdio.h>
 
@@ -7,7 +8,13 @@
 
 int main()
 {
- 
+char s[100];
 
+printf("Enter the sentence to be encrypted;");
+scanf("%[^\n]s", s);
+
+printf("The sentence is %s", s);
+ 
+return 0;
 
 }	
